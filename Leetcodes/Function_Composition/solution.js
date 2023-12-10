41774a045441dbbe3ee69bb8208ffbbda24da349
Bakhtiyor Bayjanov <89094651+bayjanov@users.noreply.github.com>
2023-12-10 22:45:01 +0100

    Update solution.js

--- Leetcodes/Function_Composition/solution.js
+++ Leetcodes/Function_Composition/solution.js
@@ -1,1 +1,16 @@
+var compose = function (functions) {
+    functions = functions.reverse();
 
+    const functionComposition = ((elem) => {
+        if (functions.length === 0) {
+            return elem;
+        }
+        
+        for (let i = 0; i < functions.length; i++) {
+            elem = functions[i](elem);
+        }
+        return elem
+    });
+
+    return functionComposition;
+};
